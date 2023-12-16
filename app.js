1f302be4728a0913c802c4dfe0867645737cfab1
Bram-Colleman <r0820751@student.thomasmore.be>
2023-12-16 11:51:50 +0100

    feat: add express

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+module.exports = app;
