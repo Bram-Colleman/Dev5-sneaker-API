6da007530a33d8c151ac14d187b34d3520e75007
TiboV <Tibo_vermeire@icloud.com>
2023-12-16 14:49:51 +0100

    feat: add route forGetAll

--- routes/shoe.js
+++ routes/shoe.js
@@ -4,5 +4,6 @@ const shoesController = require("../controllers/shoes");
 
 router.post("/", shoesController.createShoe);
 router.delete("/:id", shoesController.deleteShoe);
+router.get("/", shoesController.getShoes);
 
 module.exports = router;
