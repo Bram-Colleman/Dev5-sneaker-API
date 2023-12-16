c84d13e7fdbcc4fffc810a5d8bb934c80d5e5fe8
Bram-Colleman <r0820751@student.thomasmore.be>
2023-12-16 13:54:44 +0100

    feat: add deleteshoe route to router

--- routes/shoe.js
+++ routes/shoe.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const shoesController = require("../controllers/shoes");
 
 router.post("/", shoesController.createShoe);
+router.delete("/:id", shoesController.deleteShoe);
 
 module.exports = router;
