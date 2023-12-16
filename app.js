edc408d6342128c89b559e0899b68922790f7b12
Bram-Colleman <r0820751@student.thomasmore.be>
2023-12-16 13:01:20 +0100

    feat: add routing for index view

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
+const indexRouter = require("./routes/index");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -26,5 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
 
 module.exports = app;
