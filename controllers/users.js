66bb649514ec59b9792724a8d9ebb8567bc36b23
TiboV <Tibo_vermeire@icloud.com>
2023-12-18 13:53:04 +0100

    feat: create loginUser

--- controllers/users.js
+++ controllers/users.js
@@ -40,7 +40,53 @@ const createUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    try{
+        let user = req.body;
+        if (!user) {
+            return res.status(400).json({
+                status: "error",
+                message: "No user data provided",
+            });
+        }
+        let u = await User.findOne({userName: user.userName});
+        if (!u) {
+            return res.status(400).json({
+                status: "error",
+                message: "User not found",
+            });
+        }
+        const validPassword = await bcrypt.compare(user.password, u.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                status: "error",
+                message: "Invalid password",
+            });
+        }
+        res.json({
+            status: "success",
+            message: "User logged in successfully",
+            data: [
+                {
+                    user: u,
+                }
+            ]
+        });
+
+    } catch (err) {
+        res.json({
+            status: "error",
+            message: "POST user not successful",
+            data: [{
+                errormessage: err.message,
+            }],
+        });
+    }
+
+};
+    
 
 module.exports.createUser = createUser;
+module.exports.loginUser = loginUser;
 
 
